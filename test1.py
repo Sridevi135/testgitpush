ec9b87f139e81f4f28157fca23bf70cb788cbbc7
Sri Devi M <sridevim135@gmail.com>
2022-07-30 14:02:51 +0530

    this is my first code push in git

---
+++ test1.py
@@ -0,0 +1,16 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://sri:devisri24.@sri.jguwuwq.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+
+d={
+    'name':'sri',
+   'email':'erg@gmail',
+   'sur':'der'}
+db1=client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
+
+
